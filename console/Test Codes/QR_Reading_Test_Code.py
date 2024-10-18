bb91c6342f80bcb736ddfdfabc0fd35bd8c951cf
Ahmad Zaki <107484564+Ahmad-Zaki05@users.noreply.github.com>
2024-10-18 23:07:28 +0300

    Detecting and Decoding QR Codes in a Video
    This code detects QR codes within a live feed. However, the processing of a single frame takes a lot of time. Will have to find a solution to this issue.

--- console/Test Codes/QR_Reading_Test_Code.py
+++ console/Test Codes/QR_Reading_Test_Code.py
@@ -2,24 +2,33 @@ import cv2
 import numpy as np
 from qreader import QReader
 
-img = cv2.cvtColor(cv2.imread("QR_Sample_Multiple.png"), cv2.COLOR_BGR2RGB)
+cap = cv2.VideoCapture (1)
 
-qreader = QReader()
+while (cap.isOpened ()):
 
-decoded_text, detections = qreader.detect_and_decode(image=img, return_detections=True)
+    _, img = cap.read ();
 
-for i, qr in enumerate(decoded_text):
-    print(f"QR Code {i+1}: {qr}")
-    
-    # Get the bounding box for each QR code (as a numpy array)
-    bbox = np.int32(detections[i]["bbox_xyxy"])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    print (f"({bbox[0]}, {bbox[1]}), ({bbox[2]}, {bbox[3]})")
+    qreader = QReader()
 
-    cv2.rectangle (img, (bbox[0] - 1, bbox[1] - 1), (bbox[2] + 1, bbox[3] + 1), (0, 255, 0), 2)
+    decoded_text, detections = qreader.detect_and_decode(image=img, return_detections=True)
 
-    cv2.putText(img, qr, (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    for i, qr in enumerate(decoded_text):
+        print(f"QR Code {i+1}: {qr}")
+        
+        # Get the bounding box for each QR code (as a numpy array)
+        bbox = np.int32(detections[i]["bbox_xyxy"])
 
-cv2.imshow("Image with QR Codes", img)
-cv2.waitKey(0)
+        print (f"({bbox[0]}, {bbox[1]}), ({bbox[2]}, {bbox[3]})")
+
+        cv2.rectangle (img, (bbox[0] - 1, bbox[1] - 1), (bbox[2] + 1, bbox[3] + 1), (0, 255, 0), 2)
+
+        cv2.putText(img, qr, (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    cv2.imshow("Image with QR Codes", img)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
