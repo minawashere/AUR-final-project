861462fca19798e87aa632f596f360de1d527ac8
minawashere <minasalama2580@gmail.com>
2024-10-26 04:45:03 +0300

    created branch embedded

--- embedded/embedded-arduino-framework/src/main.cpp
+++ embedded/embedded-arduino-framework/src/main.cpp
@@ -6,13 +6,9 @@ Encoder encoder(2 , 4);
 
 
 void setup() {
-    Serial.begin(115200);
-    encoder.begin();
-    pinMode(5, OUTPUT);
-    analogWrite(5, 100);
+
 }
 
 void loop() {
-    Serial.println(encoder.get());
-    delay(200);
+
 }
