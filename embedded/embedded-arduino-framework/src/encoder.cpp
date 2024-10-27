abbc10844676546903e341ad5e1a9dbf20c6674f
minawashere <minasalama2580@gmail.com>
2024-10-27 20:22:45 +0300

    generalized encoders

--- embedded/embedded-arduino-framework/src/encoder.cpp
+++ embedded/embedded-arduino-framework/src/encoder.cpp
@@ -1,94 +1,28 @@
 #include "encoder.h"
 
-#define PPR 22  // Pulses per revolution
+#include <float.h>
 
-// Static variable definitions
-volatile int encoderPosition = 0;
-volatile int encoderDirection = 0;
-volatile int pulseCount = 0;
-Encoder* Encoder::instance = nullptr; // Initialize the static instance pointer
-volatile unsigned long last_time = 0;
+#define PULSES_PER_REV 11
+#define GEAR_RATIO 46.8
 
-Encoder::Encoder(const int pinA, const int pinB) : pinA(pinA), pinB(pinB) {
-    last_time = micros();
-}
 
-void Encoder::begin() {
-    Serial.println("Initializing encoder...");
-
+Encoder::Encoder(const uint8_t pinA, const uint8_t pinB, void (*isr)()) : pinA(pinA), pinB(pinB) {
     pinMode(pinA, INPUT);
     pinMode(pinB, INPUT);
 
-    instance = this;
-
-    attachInterrupt(digitalPinToInterrupt(pinA), encoderISR_A, RISING);
-    attachInterrupt(digitalPinToInterrupt(pinB), encoderISR_B, RISING);
-
-    Serial.println("Encoder initialized.");
+    attachInterrupt(digitalPinToInterrupt(pinA), isr, RISING);
 }
 
-int Encoder::getPosition() {
 
-    noInterrupts();
-    int position = encoderPosition;
-    interrupts();
-    return position;
-}
-
-bool Encoder::getDirection() {
-    // noInterrupts();
-    // bool direction = encoderDirection;
-    // interrupts();
-    return encoderDirection;
-}
-
-int Encoder::getPulseCount(){
-    // noInterrupts();
-    // int count = pulseCount;
-    // interrupts();
-    return pulseCount;
-}
-
-void Encoder::encoderISR_A() {
-    if (instance) {
-        bool pinAState = digitalRead(instance->pinA);
-        bool pinBState = digitalRead(instance->pinB);
-
-        if (pinAState != pinBState) {
-            encoderPosition++;
-            encoderDirection = 1;
-        } else {
-            encoderPosition--;
-            encoderDirection = 0;
-        }
-        pulseCount++;
-    }
-}
-
-void Encoder::encoderISR_B() {
-    if (instance) {
-        bool pinAState = digitalRead(instance->pinA);
-        bool pinBState = digitalRead(instance->pinB);
-
-        if (pinAState == pinBState) {
-            encoderPosition++;
-            encoderDirection = 1;
-        } else {
-            encoderPosition--;
-            encoderDirection = 0;
-        }
-        pulseCount++;
-    }
-}
-
-float Encoder::calcRPM() {
+double Encoder::rpm() {
     const unsigned long time = micros();
-    const double dt = (time - last_time) / 1000000.0;
-    if (dt > 0) {
-        rpm = (pulseCount - last_pulses)  * 60.0 / dt / PPR / 46.8;
-    }
+    const double dt = (time - prev_time) / 1000000.0;
+    const uint32_t dp = pulse_count() - prev_pulses;
 
-    last_time = time;
-    last_pulses = pulseCount;
-    return rpm;
+    this->prev_time = time;
+    this->prev_pulses = pulse_count();
+
+    if (dt < DBL_EPSILON) return prev_rpm;
+    return dp / dt * 60 / PULSES_PER_REV / GEAR_RATIO * static_cast<double>(direction());
 }
+
